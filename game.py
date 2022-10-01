a830d82ee6703a4abfd93bf2015e96a122a16030
mjq4my <michael.quinn808@gmail.com>
2022-10-01 12:09:12 -0400

    updated zombie game

--- game.py
+++ game.py
@@ -3,6 +3,7 @@
 import pygame
 import gamebox
 import random
+import os
 
 camera = gamebox.Camera(1200,700)
 
@@ -15,7 +16,7 @@ start_pic = gamebox.from_image(600,350,"http://img08.deviantart.net/e319/i/2013/
 
 intro = gamebox.from_text(camera.x, 100, "Welcome to Zombies", "Impact", 60, "white")
 instructions = gamebox.from_text(camera.x, 600, "Instructions: Press up and down to move, space to shoot. Defeat all three waves of zombies.", "Impact", 30, "white")
-names = gamebox.from_text(camera.x, 170, "Michael Quinn (mjq4my) & Daniel Forsman (dhf5qe)", "Impact", 30, "white")
+# names = gamebox.from_text(camera.x, 170, "Michael Quinn (mjq4my) & Daniel Forsman (dhf5qe)", "Impact", 30, "white")
 starting = gamebox.from_text(camera.x, 650, "Press s to start", "Impact", 40, "white")
 wave1 = gamebox.from_text(camera.x, 50, "WAVE 1", "Impact", 30, "red")
 wave2 = gamebox.from_text(camera.x, 50, "WAVE 2", "Impact", 30, "red")
@@ -35,9 +36,11 @@ first_zombies =[]
 second_zombies = []
 last_zombies = []
 bullets = []
-# gunshot = gamebox.load_sound("gun_fire.wav")
-# reload = gamebox.load_sound("gun_cocking.wav")
-# zombiedie = gamebox.load_sound("zombie_die.wav")
+
+gunshot = gamebox.load_sound("sounds/gunshot01.wav")
+# reload = gamebox.load_sound("sounds/gun_cocking.wav")
+zombiedie = gamebox.load_sound("sounds/zombie_attack.wav")
+
 ammo = 100
 zombie_speed = 4
 shooter_speed=10
@@ -59,24 +62,33 @@ for z in range(25):
     y = random.randrange(50,650)
     last_zombies.append(gamebox.from_image(x,y,"pics/zombie.png"))
 
+def reload():
+    one = gamebox.load_sound("sounds/outofammo.wav")
+    one.play()
+    two = gamebox.load_sound("sounds/weapload.wav")
+    two.play()
+    return
+
 def losegame(keys):
     camera.clear("white")
     camera.draw(lose_pic)
     camera.draw(game_over)
-    # reload.stop()
-    # gunshot.stop()
+    reload()
+    gunshot.stop()
     camera.display()
+
 def win_game(keys):
     camera.draw(win_pic)
     camera.draw(game_winner)
-    # reload.stop()
-    # gunshot.stop()
+    reload()
+    gunshot.stop()
     camera.display()
+
 def tick(keys):
     global frame
     global game_on, lose_game
     global ammo
-    frames = str(int(frame/22))                         #approximate time in seconds
+    frames = str(int(frame/22))                         # approximate time in seconds
     timer = gamebox.from_text(900, 50, "Timer: " + frames, 'impact', 30, 'red')
     if game_on == False:
         camera.clear("white")
@@ -84,11 +96,11 @@ def tick(keys):
         camera.draw(intro)
         camera.draw(instructions)
         camera.draw(starting)
-        camera.draw(names)
+        # camera.draw(names)
         camera.display()
         if pygame.K_s in keys:
             game_on = True
-    if game_on == True:                                 #start game
+    if game_on == True:                                 # start game
         frame+=1
         camera.clear("grey")
         camera.draw(backgr)
@@ -96,77 +108,77 @@ def tick(keys):
         camera.draw(ammo_str)
         camera.draw(shooter)
         camera.draw(timer)
-        if shooter.y <= 100:                            #shooter boundaries
+        if shooter.y <= 100:                            # shooter boundaries
             shooter.y = 100
         if shooter.y >=650:
             shooter.y = 650
-        if len(first_zombies)!=0:                       #Setting up wave number
+        if len(first_zombies)!=0:                       # Setting up wave number
             camera.draw(wave1)
         elif len(first_zombies)==0 and len(second_zombies)!=0:
             camera.draw(wave2)
         else:
             camera.draw(finalwave)
-        for zombie in first_zombies:                    #if bullet touches zombie, kill zombie
+        for zombie in first_zombies:                    # if bullet touches zombie, kill zombie
             for bullet in bullets:
                 if bullet.touches(zombie) and zombie.x<=1230:
                     first_zombies.remove(zombie)
-                    # zombiedie.play()
+                    zombiedie.play()
                     bullets.remove(bullet)
         for zombie in second_zombies:
             for bullet in bullets:
                 if bullet.touches(zombie) and zombie.x<=1230:
                     second_zombies.remove(zombie)
-                    # zombiedie.play()
+                    zombiedie.play()
                     bullets.remove(bullet)
         for zombie in last_zombies:
             for bullet in bullets:
                 if bullet.touches(zombie) and zombie.x<=1230:
                     last_zombies.remove(zombie)
-                    # zombiedie.play()
+                    zombiedie.play()
                     bullets.remove(bullet)
         for zombie in first_zombies:
             zombie.size = 80, 130
             camera.draw(zombie)
             zombie.x-=zombie_speed
-            if frame%10 == 1:                           #animation
-                zombie.image = "go_1.png"
+            if frame%10 == 1:                           # animation
+                zombie.image = os.getcwd()+"/pics/zombies/walk/go_1.png"
             elif frame%10 == 2:
-                zombie.image = "go_2.png"
+                zombie.image = os.getcwd()+"/pics/zombies/walk/go_2.png"
             elif frame%10 == 3:
-                zombie.image = "go_3.png"
+                zombie.image = os.getcwd()+"/pics/zombies/walk/go_3.png"
             elif frame%10 == 4:
-                zombie.image = "go_4.png"
+                zombie.image = os.getcwd()+"/pics/zombies/walk/go_4.png"
             elif frame % 10 == 5:
-                zombie.image = "go_5.png"
+                zombie.image = os.getcwd()+"/pics/zombies/walk/go_5.png"
             elif frame%10 == 6:
-                zombie.image = "go_6.png"
+                zombie.image = os.getcwd()+"/pics/zombies/walk/go_6.png"
             elif frame%10 == 7:
-                zombie.image = "go_7.png"
+                zombie.image = os.getcwd()+"/pics/zombies/walk/go_7.png"
             elif frame%10 == 8:
-                zombie.image = "go_8.png"
+                zombie.image = os.getcwd()+"/pics/zombies/walk/go_8.png"
             elif frame%10 == 9:
-                zombie.image = "go_9.png"
+                zombie.image = os.getcwd()+"/pics/zombies/walk/go_9.png"
         if pygame.K_UP in keys:
             shooter.y -= shooter_speed
         if pygame.K_DOWN in keys:
             shooter.y += shooter_speed
         if pygame.K_SPACE in keys:
             bullet = gamebox.from_color(150, shooter.y - 55, "orange", 10, 2)
-            # gunshot.play()
+            gunshot.play()
             bullets.append(bullet)
             ammo-=1
             if ammo%10==0 and ammo>0:
-                for a in range(1):             #make reload sound after every 10 shots
-                    # reload.play()
+                for a in range(1):             # make reload sound after every 10 shots
+                    reload()
                     keys.clear()
                     break
             keys.clear()
-        if ammo<=0:                             #no more ammo
+        if ammo<=0:                             # no more ammo
             bullets. clear()
-            # reload.stop()
-            # gunshot.stop()
+            reload()
+            gunshot.stop()
             ammo =0
-        for bullet in bullets:                  #shoot the bullets
+        for bullet in bullets:                  # shoot the bullets
             bullet.x+=80
             camera.draw(bullet)
         for zombie in first_zombies:
@@ -184,46 +196,46 @@ def tick(keys):
                 camera.draw(zombie)
                 zombie.x -= zombie_speed +1
                 if frame % 10 == 1:
-                    zombie.image = "go_1.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_1.png"
                 elif frame % 10 == 2:
-                    zombie.image = "go_2.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_2.png"
                 elif frame % 10 == 3:
-                    zombie.image = "go_3.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_3.png"
                 elif frame % 10 == 4:
-                    zombie.image = "go_4.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_4.png"
                 elif frame % 10 == 5:
-                    zombie.image = "go_5.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_5.png"
                 elif frame % 10 == 6:
-                    zombie.image = "go_6.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_6.png"
                 elif frame % 10 == 7:
-                    zombie.image = "go_7.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_7.png"
                 elif frame % 10 == 8:
-                    zombie.image = "go_8.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_8.png"
                 elif frame % 10 == 9:
-                    zombie.image = "go_9.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_9.png"
         if len(second_zombies)==0:
             for zombie in last_zombies:
                 zombie.size = 80, 130
                 camera.draw(zombie)
                 zombie.x -= zombie_speed +2
                 if frame % 10 == 1:
-                    zombie.image = "go_1.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_1.png"
                 elif frame % 10 == 2:
-                    zombie.image = "go_2.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_2.png"
                 elif frame % 10 == 3:
-                    zombie.image = "go_3.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_3.png"
                 elif frame % 10 == 4:
-                    zombie.image = "go_4.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_4.png"
                 elif frame % 10 == 5:
-                    zombie.image = "go_5.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_5.png"
                 elif frame % 10 == 6:
-                    zombie.image = "go_6.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_6.png"
                 elif frame % 10 == 7:
-                    zombie.image = "go_7.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_7.png"
                 elif frame % 10 == 8:
-                    zombie.image = "go_8.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_8.png"
                 elif frame % 10 == 9:
-                    zombie.image = "go_9.png"
+                    zombie.image = os.getcwd()+"/pics/zombies/walk/go_9.png"
         if lose_game==True:
             losegame(keys)
             first_zombies.clear()
